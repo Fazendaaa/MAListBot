eaf27c5c0229dd6d5d217091566afd093d823fd1
Farm <lucas.carotta@outlook.com>
2017-04-05 15:27:44 -0300

    Adding new layout and plot button.

--- bot/mal.js
+++ bot/mal.js
@@ -14,8 +14,7 @@ const help = "Usage:\n\n\
 /character \'character name\'\n\
 /source -- see the code behind MAListbot\n\n\
 Any bugs or suggestions, talk to: @farmy"
-const defaultResponse = 'Please, feel free to search MAL. This query\
-will reply any given aspect of the search as Characters, Animes and Mangas.'
+const defaultResponse = 'Please, feel free to search MAL.'
 
 bot.command( 'start', ctx => {
 	ctx.reply( welcome )
@@ -75,31 +74,95 @@ function verifyData( data, unit ) {
 		   `${unit}${data}` : 'Not avaliable'
 }
 
-function replyMarkdown( data ) {
-	const score = verifyData( data.score, '' )
-	const ranked = verifyData( data.ranked, '#' )
-	const popularity = verifyData( data.popularity, '#' )
-
-	return `[${data.title}](${data.mal.url + data.path})
-- _Ranked_: *${ranked}*
-- _Popularity_: *${popularity}*
-- _Score_: *${score}*`
+function verifyDataMd( pre, data, unit ) {
+	return ( null != data && undefined != data ) ?
+		   `- _${pre}_: *${unit}${data}*\n` : ''
 }
+
+function replyMarkdown( data ) {
+	const score = verifyDataMd( 'Score', data.score, '' )
+	const ranked = verifyDataMd( 'Ranked', data.ranked, '#' )
+	const popularity = verifyDataMd( 'Popularity	', data.popularity, '#' )
+
+	return `[\u200B](${data.cover})[${data.title}](${data.mal.url + data.path})
+${ranked}${popularity}${score}`
+}
+
+bot.action( /.+/, ( ctx ) => {
+	return ctx.answerCallbackQuery( ctx.match[ 0 ], undefined, true )  
+})
+
+function replyButton( plot ) {
+	return Telegraf.Extra
+				   .markdown( )
+				   .markup( ( m ) =>
+				      m.inlineKeyboard( [
+					      m.callbackButton( 'Plot', plot.substring( 0, 40 ) )
+				   ] )
+	).reply_markup
+}
+
+/*
+Title {
+  mal: MyAnimeList { url: 'https://myanimelist.net' },
+  type: 'anime',
+  id: '813',
+  sn: 'Dragon_Ball_Z',
+  path: '/anime/813/Dragon_Ball_Z',
+  fetched: true,
+  title: 'Dragon Ball Z',
+  score: 8.32,
+  ranked: 212,
+  popularity: 62,
+  members: 5,
+  cover: 'https://myanimelist.cdn-dena.com/images/anime/6/20936.jpg',
+  description: 'Five years after winning the World Martial Arts tournament,
+  Gokuu is now living a peaceful life with his wife and son. This changes,
+  however, with the arrival of a mysterious enemy named Raditz who presents
+  himself as Gokuu\'s long-lost brother. He reveals that Gokuu is a warrior
+  from the once powerful but now virtually extinct Saiyan race, whose
+  homeworld was completely annihilated. When he was sent to Earth as a baby,
+  Gokuu\'s sole purpose was to conquer and destroy the planet; but after
+  suffering amnesia from a head injury, his violent and savage nature changed,
+  and instead was raised as a kind and well-mannered boy, now fighting to
+  protect others.\r\n\r\nWith his failed attempt at forcibly recruiting Gokuu as
+  an ally, Raditz warns Gokuu\'s friends of a new threat that\'s rapidly
+  approaching Earth—one that could plunge Earth into an intergalactic conflict
+  and cause the heavens themselves to shake. A war will be fought over the seven
+  mystical dragon balls, and only the strongest will survive in Dragon Ball Z.
+  \r\n\r\n[Written by MAL Rewrite]',
+  pictures: 
+   [ 'https://myanimelist.cdn-dena.com/images/anime/13/8309l.jpg',
+     'https://myanimelist.cdn-dena.com/images/anime/10/16450l.jpg',
+     'https://myanimelist.cdn-dena.com/images/anime/13/18075l.jpg',
+     'https://myanimelist.cdn-dena.com/images/anime/6/20936l.jpg',
+     'https://myanimelist.cdn-dena.com/images/anime/8/22303l.jpg',
+     'https://myanimelist.cdn-dena.com/images/anime/12/71549l.jpg',
+     'https://myanimelist.cdn-dena.com/images/anime/8/71550l.jpg',
+     'https://myanimelist.cdn-dena.com/images/anime/10/80325l.jpg',
+     'https://myanimelist.cdn-dena.com/images/anime/8/80326l.jpg' ],
+  characters: 
+
+*/
 
 /*	Telegram  return  all  data  for  inline request as a JSON, replyInline only
 	takes  all  info  recieved  through quickSearch and 'cleans it' to put it in
 	Telegram standars
 */
 function replyInline( data ) {
+	const description = verifyData( data.description, '' )
+
 	return {
 		id: data.id,
 		title: '[' + data.type.toUpperCase() + '] ' + data.title,
 		type: 'article',
 		input_message_content: {
 			message_text: replyMarkdown( data ),
-			parse_mode: 'Markdown'
+			parse_mode: 'Markdown',
+			disable_web_page_preview: false
 		},
-		description: data.description,
+		reply_markup: replyButton( data.description ),
+		description: description,
 		thumb_url: data.cover
 	}
 }
